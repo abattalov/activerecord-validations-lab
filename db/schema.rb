4aa6aacd93c1cdca38c2debf2c45029c92a6535b
github username <github email address>
2022-02-22 22:41:44 -0600

    commit tree 6723bceb76e879652ee152d901b3319fa050aba1

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151202061742) do
     t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    
   end
 
 end
